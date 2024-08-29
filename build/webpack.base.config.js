84ad5d6b0a88be45a324ec1262a7524de19a7c1e
Book Pauk <bookpauk@gmail.com>
2024-08-29 17:44:43 +0700

    Добавил define

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -61,6 +61,7 @@ module.exports = {
         new DefinePlugin({
             __VUE_OPTIONS_API__: true,
             __VUE_PROD_DEVTOOLS__: false,
+            __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: true,
             __QUASAR_SSR__: false,
             __QUASAR_SSR_SERVER__: false,
             __QUASAR_SSR_CLIENT__: false,
